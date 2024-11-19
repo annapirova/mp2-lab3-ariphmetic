551c4dd855f60c63984c0862c897d3b8f4e5604f
NikaKrasnopevtseva <144881960+NikaKrasnopevtseva@users.noreply.github.com>
2024-11-19 23:16:51 +0300

    Add files via upload

--- samples/main_arithmetic.cpp
+++ samples/main_arithmetic.cpp
@@ -1,6 +1,31 @@
 // реализация пользовательского приложения
+#include "stack.h"
+#include "arithmetic.h"
+using namespace std;
 
-int main()
-{
-  return 0;
+int main() {
+    setlocale(LC_ALL, "Russian");
+    string in;
+    std::cout << "enter expression : ";
+    getline(cin, in);
+    std::cout << std::endl;
+    Arithmetic expr(in);
+    std::cout << expr.GetInfix() << std::endl;
+    cout << endl;
+    std::cout << expr.GetPostfix() << std::endl;
+    expr.PrintPostfixLexems();
+    vector<string> operands = expr.GetOperands();
+    double val;
+    map<string, double> values;
+    for (const auto& op : operands) {
+        if (GetType(op) == 2)
+            values[op] = std::stod(op);
+        else {
+            std::cout << "enter value of " << op << ": ";
+            std::cin >> val;
+            values[op] = val;
+        }
+    }
+    cout << "result: " << expr.Calculate(values) << endl;
+    return 0;
 }
